e50128160dc5c2b6a353752799ea31e69b094e92
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-22 09:13:45 +0000

    updated core data properties

--- Countries/CoreData/Country+CoreDataProperties.swift
+++ Countries/CoreData/Country+CoreDataProperties.swift
@@ -18,6 +18,8 @@ extension Country {
 
     @NSManaged public var capital: String?
     @NSManaged public var name: String?
+    @NSManaged public var area: Int32
+    @NSManaged public var region: String?
     @NSManaged public var population: Int32
 
 }
