5f35fc80880eb499a027b19cc7ceae96545f891e
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-22 09:16:53 +0000

    added more outlets

--- Countries/UI/CountryTableViewCell.swift
+++ Countries/UI/CountryTableViewCell.swift
@@ -9,23 +9,13 @@
 import UIKit
 
 class CountryTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var country: UILabel!
-    @IBOutlet weak var capital: UILabel!
-    @IBOutlet weak var capitalLabel: UILabel!
-    @IBOutlet weak var capitalStackView: UIStackView!
-    @IBOutlet weak var population: UILabel!
-    @IBOutlet weak var populationLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var areaLabel: UILabel!
+    
+  
+    
 }
