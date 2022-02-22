b992047470d2219abfbc43779798676a60977f6f
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-22 11:52:33 +0000

    added labels to UI tests to help identifier them

--- CountriesUITests/CountriesUITests.swift
+++ CountriesUITests/CountriesUITests.swift
@@ -12,6 +12,19 @@ class CountriesUITests: XCTestCase {
 
     let kTimeOut = 10.0
     
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        //sleep(1)
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
     func testPopulationFormat() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -23,17 +36,17 @@ class CountriesUITests: XCTestCase {
         //   1,000,000,000 <= China population <= 999,999,999,999
         //   1,000 <= Antarctica population <= 9,999
         
-        let afghanPopulation = app.staticTexts["Afghanistan-Population"]
+        let afghanPopulation = app.staticTexts["Afghanistan-Population-Label"]
         XCTAssertTrue(afghanPopulation.waitForExistence(timeout: kTimeOut))
         var commas = afghanPopulation.label.filter({return $0 == ","}).count
         XCTAssertEqual(2, commas)
         
-        let chinaPopulation = app.staticTexts["China-Population"]
+        let chinaPopulation = app.staticTexts["China-Population-Label"]
         XCTAssertTrue(afghanPopulation.waitForExistence(timeout: kTimeOut))
         commas = chinaPopulation.label.filter({return $0 == ","}).count
         XCTAssertEqual(3, commas)
         
-        let antarcticaPopulation = app.staticTexts["Antarctica-Population"]
+        let antarcticaPopulation = app.staticTexts["Antarctica-Population-Label"]
         XCTAssertTrue(antarcticaPopulation.waitForExistence(timeout: kTimeOut))
         commas = antarcticaPopulation.label.filter({return $0 == ","}).count
         XCTAssertEqual(1, commas)
@@ -47,11 +60,11 @@ class CountriesUITests: XCTestCase {
         // The purpose of this test is to check cpital cities are correct,
         // and that we don't try and display non-existent capitals, such as for Antarctica.
         
-        let algeriaCapital = app.staticTexts["Algeria-Capital"]
+        let algeriaCapital = app.staticTexts["Algeria-Capital-Label"]
         XCTAssertTrue(algeriaCapital.waitForExistence(timeout: kTimeOut))
         XCTAssertEqual("Algiers", algeriaCapital.label)
         
-        let andorraCapital = app.staticTexts["Andorra-Capital"]
+        let andorraCapital = app.staticTexts["Andorra-Capital-Label"]
         XCTAssertTrue(andorraCapital.waitForExistence(timeout: kTimeOut))
         XCTAssertEqual("Andorra la Vella", andorraCapital.label)
 
