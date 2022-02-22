1fb0efeab30e231eeaba98ba22a5fb497d00f05b
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-22 09:05:31 +0000

    remove unused overrides

--- CountriesUITests/CountriesUITests.swift
+++ CountriesUITests/CountriesUITests.swift
@@ -12,19 +12,6 @@ class CountriesUITests: XCTestCase {
 
     let kTimeOut = 10.0
     
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        sleep(1)
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testPopulationFormat() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
